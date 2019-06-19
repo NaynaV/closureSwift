470481c67ad721925183dd602ce1b0a1676b47f3
nayna <nainavaghasiya@gmail.com>
2019-06-19 19:12:16 -0400

    functions added

--- closureSwift/main.swift
+++ closureSwift/main.swift
@@ -47,3 +47,44 @@ print("m\(m)")
 
 funcAdd = mul
 print ("mul : \(funcAdd(2,5))")
+
+
+let names = ["Chris", "Alex", "Ewa", "Barry", "Daniella"]
+func backward(_ s1: String, _ s2: String) -> Bool {
+    return s1 > s2
+}
+var reversedNames = names.sorted(by: backward)
+// reversedNames is equal to ["Ewa", "Daniella", "Chris", "Barry", "Alex"]
+print(names)
+var reversedNAmes = names.sorted(by : backward)
+print(reversedNAmes)
+
+reversedNAmes = names.sorted(by:
+    {
+        (a:String, b : String)-> Bool in
+        return a > b
+})
+print(reversedNAmes)
+
+
+reversedNAmes = names.sorted(by:
+    {
+        
+        return $0 > $1
+})
+print(reversedNAmes)
+
+reversedNAmes = names.sorted(by:
+    {
+        
+        $0 > $1
+})
+print(reversedNAmes)
+
+
+reversedNAmes = names.sorted(by: >)
+print(reversedNAmes)
+
+
+reversedNAmes = names.sorted(by: <)
+print(reversedNAmes)
